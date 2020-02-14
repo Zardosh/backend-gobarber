d6e16ab1f31bbfeac4385e48029ff135f3ae6ba4
Eduardo Zardo <eduzarrdo@gmail.com>
2020-02-14 18:22:31 -0300

    First commit

---
+++ src/config/authExample.js
@@ -0,0 +1,18 @@
+/*
+
+Esse é um exemplo de como configurar o sistema
+de sessões. Mude o valor do campo "secret" para
+o seu segredo. Caso deseje, você pode usar o
+link a seguir para gerar um:
+https://www.md5online.org/
+
+Após configurar, remova os comentários e renomeie o arquivo para "auth.js".
+
+*/
+
+/*
+export default {
+    secret: '[Insira seu segredo aqui]',
+    expiresIn: '7d',
+};
+*/
---
+++ src/config/databaseExample.js
@@ -0,0 +1,29 @@
+/*
+
+Esse é um exemplo de como fazer a configuração do
+banco de dados. Mude os campos de "username" e
+"password" para o seu usuário e senha no banco,
+respectivamente.
+
+Caso você queira usar algo diferente do PostgreSQL,
+mude o valor do campo "dialect" para o nome do SGBD
+desejado. Para consultar os SGBDs disponíveis, vá para:
+https://sequelize.readthedocs.io/en/1.7.0/docs/usage/#dialects
+
+Após configurar, remova os comentários e renomeie o arquivo para "database.js".
+*/
+
+/*
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: '[Usuario no banco]',
+    password: '[Senha no banco]',
+    database: '[Nome da base de dados]',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
+*/
